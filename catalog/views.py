26369fe235be8a3b785a08099907c80eb3bf9f99
Alexandr Zibinin <zibinin.chel@gmail.com>
2024-05-04 20:49:25 +0300

    1

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,12 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        with open('answer.txt', mode='w') as f:
+            f.write(name + '\n')
+            f.write(phone + '\n')
+            f.write(message + '\n')
     return render(request, 'contacts.html')
